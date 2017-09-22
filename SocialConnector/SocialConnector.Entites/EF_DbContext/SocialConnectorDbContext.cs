1a6c888e71a11a99b9199654a760f35ed0f8f0e5
Mayher Maryan <mr.maykher@gmail.com>
2017-09-22 16:23:02 +0300

    Added EF and appsettings

---
+++ SocialConnector/SocialConnector.Entites/EF_DbContext/SocialConnectorDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocialConnector.Entites.EF_DbContext
+{
+    public class SocialConnectorDbContext : DbContext
+    {
+        public SocialConnectorDbContext(DbContextOptions<SocialConnectorDbContext> options) : base(options)
+        {
+                
+        }
+    }
+}
--- SocialConnector/SocialConnector.Web/Startup.cs
+++ SocialConnector/SocialConnector.Web/Startup.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using SocialConnector.Entites.EF_DbContext;
 
 namespace SocialConnector.Web
 {
@@ -13,8 +17,23 @@ namespace SocialConnector.Web
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
         public void ConfigureServices(IServiceCollection services)
         {
+            string conncetion = Configuration.GetConnectionString("SocialConnectorDb");
+
+            services.AddDbContext<SocialConnectorDbContext>(options => options.UseSqlServer(conncetion));
+
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options =>
+            {
+                options.LoginPath = new PathString("/Account/Login");
+            });
+
             services.AddMvc();
         }
 
@@ -24,8 +43,12 @@ namespace SocialConnector.Web
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
             }
             app.UseStaticFiles();
+
+            app.UseAuthentication();
+
             app.UseMvc(routes =>
             {
                 routes.MapRoute("default", "{controller=Home}/{action=Profile}/{id?}");
