0e530a669edf0e21f0800251f63bcf639483a6af
Mayher Maryan <mr.maykher@gmail.com>
2017-09-23 11:18:51 +0300

    provided authentication

--- SocialConnector/SocialConnector.Web/Controllers/AccountController.cs
+++ SocialConnector/SocialConnector.Web/Controllers/AccountController.cs
@@ -3,10 +3,86 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SocialConnector.Entites.EF_DbContext;
+using SocialConnector.Web.ViewModels.Authentication;
+using SocialConnector.Entites.Entities;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 namespace SocialConnector.Web.Controllers
 {
-    public class AccountController :Controller
+    public class AccountController : Controller
     {
+        private SocialConnectorDbContext db;
+        public AccountController(SocialConnectorDbContext context)
+        {
+            db = context;
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = db.Users.FirstOrDefault(u => u.Email == model.LoginName && u.Password == model.Password);
+                if (user != null)
+                {
+                    Authenticate(model.LoginName); // аутентификация
+
+                    return RedirectToAction("Index", "Home");
+                }
+                ModelState.AddModelError("", "Incorrect login or password");
+            }
+            return View(model);
+        }
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Register(RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = db.Users.FirstOrDefault(u => u.Email == model.Email);
+                if (user == null)
+                {
+                    // добавляем пользователя в бд
+                    db.Users.Add(new User { Email = model.Email, Password = model.Password });
+                    db.SaveChanges();
+
+                    Authenticate(model.Email); // аутентификация
+
+                    return RedirectToAction("Profile", "Home");
+                }
+                else
+                    ModelState.AddModelError("", "Incorrect login or password");
+            }
+            return View(model);
+        }
+
+        private void Authenticate(string userName)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimsIdentity.DefaultNameClaimType, userName)
+            };
+            ClaimsIdentity id = new ClaimsIdentity(claims, "ApplicationCookie", ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType);
+            HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(id));
+        }
+
+        public IActionResult Logout()
+        {
+            HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Login", "Account");
+        }
     }
 }
